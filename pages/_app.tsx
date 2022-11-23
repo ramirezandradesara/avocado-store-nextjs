132acb54d2f378a6bdb76be3f08c9433b79b5e4e
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2022-11-23 20:24:15 -0300

    14. path alias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import App from "next/app"
-import Layout from "components/Layout/Layout"
+import Layout from "@components/Layout/Layout"
 
 export default function MyApp({ Component, pageProps }) {
-
     // providers Context/Providers, ytheme, data
     // Layout, si es comun a todas las páginas
     // props adicionales
