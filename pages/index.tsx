6402f261fac3bb70bca01a8c86bf159fc7e459ce
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2022-11-29 16:43:46 -0300

    26. nextjs image

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,11 +27,10 @@ const Home = () => {
     <div>
       <h1 className="text-3xl font-bold text-center"> Welcome to Avo Store! </h1>
       {productList?.map((product) => (
-
-        <div className='lg:flex justify-center gap-10'>
+        <div className='lg:flex justify-center gap-10' key={product.id}>
           <div className='flex-column shadow-lg p-8 rounded-xl m-5' onClick={() => navigateProductDetails(product.id)}>
-            <Image src={product.image} alt={product.id} width={300} height={300} />
-            <div key={product.id}>{product.name}</div>
+            <Image src={product.image} alt={product.id} width={300} height={300} priority={product.image === "/images/maluma.jpg" ? true : false} />
+            <div >{product.name}</div>
             <div>Price: {product?.price}</div>
           </div>
         </div>
