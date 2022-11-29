8a82959054d45f8f16008cb1607bf9f4d66ad6c4
Sara Ramírez Andrade <sararamirezandrade@gmail.com>
2022-11-29 11:56:36 -0300

    19. getServerSideProps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { Inter } from '@next/font/google'
 
 export default function Document() {
     return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,31 +2,44 @@ import React, { useEffect, useState } from 'react'
 import Image from "next/image";
 import { useRouter } from 'next/router';
 
+//va a suceder en el servidor
+export const getServerSideProps = async () => {
+ // Only absolute URLs are supported --> const response = await fetch('/api/avo') ✖️
+  // const response = await fetch('/api/avo')
+  const response = await fetch('https://avocado-store-nextjs.vercel.app/api/avo')
+  const { data: productListProps }: TAPIAvoResponse = await response.json() // hacemos rename
 
-const Home = () => {
+  return {
+    props: {
+      productListProps
+    }
+  }
+}
+
+const Home = ({ productListProps }: { productListProps: TProduct[] }) => {
 
   const router = useRouter()
 
-  const [productList, setProductList] = useState<TProduct[]>([])
+  // const [productList, setProductList] = useState<TProduct[]>([])
 
-  useEffect(() => {
-    window
-      .fetch('/api/avo')
-      .then(response => response.json())
-      .then(({ data, length }) => {
-        setProductList(data)
-      })
+  // useEffect(() => {
+  //   window
+  //   fetch('/api/avo')
+  //     .then(response => response.json())
+  //     .then(({ data, length }) => {
+  //       setProductList(data)
+  //     })
 
-  }, [])
+  // }, [])
 
   function navigateProductDetails(id: string) {
     router.push(`/product/${id}`)
-  }
+  };
 
   return (
     <div>
       <h1 className="text-3xl font-bold text-center"> Welcome to Avo Store! </h1>
-      {productList?.map((product) => (
+      {productListProps?.map((product) => (
 
         <div className='lg:flex justify-center gap-10'>
           <div className='flex-column shadow-lg p-8 rounded-xl m-5' onClick={() => navigateProductDetails(product.id)}>
@@ -35,7 +48,6 @@ const Home = () => {
             <div>Price: {product?.price}</div>
           </div>
         </div>
-
       ))}
     </div>
   )
